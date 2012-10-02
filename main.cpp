1e9e9014a0eda9c9f922d117dd9632e1eeaa82fc
Stasys Jurksaitis <stasys.jurksaitis@gmail.com>
2012-10-02 17:45:56 +0300

    Realizavau sudeties operacija

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
+int sudetis(int x, int x){
+	return x + y;
+}
 int main (int argc, char const *argv[])
 {
-	cout << "Sveika aritmetika!";
+	int x = 10, y = 5;
+	cout << "Sveika aritmetika!"<<endl;
+	cout << "Sudetis"<<sudetis(x, y)<<endl;
 	return 0;
 }
